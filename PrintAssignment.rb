c3c77298b79db407cb1ef3392f32951f8b0c9cec
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-25 11:17:29 -0400

    First commit

---
+++ PrintAssignment.rb
@@ -0,0 +1,7 @@
+= begin
+this program will display numbers from 1 to 100 inclusive, with
+all numbers evenly divisible by 3 displayed as THREE and all numbers
+evenly divisible by 5 displayed as FIVE
+in addition, if any number is divisible by both 3 and 5 the result will display
+SUCCESS
+= end
---
+++ SuccessFunction.rb
@@ -0,0 +1,13 @@
+def sucess(number)
+	if number % 3 == 0 && number = % 5 == 0
+	"SUCCESS!"
+	elsif number % 3 == 0
+	"THREE"
+	elsif number % 5 == 0
+	"FIVE"
+	else
+	number
+
+	end
+
+end
