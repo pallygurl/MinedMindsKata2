f73dd636eb9b8a8299fad87a7a865a7a2cb9281e
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-29 10:40:28 -0400

    Print to screen complete

--- PrintAssignment.rb
+++ PrintAssignment.rb
@@ -1,7 +1,24 @@
-= begin
+=begin
 this program will display numbers from 1 to 100 inclusive, with
 all numbers evenly divisible by 3 displayed as THREE and all numbers
 evenly divisible by 5 displayed as FIVE
 in addition, if any number is divisible by both 3 and 5 the result will display
-SUCCESS
-= end
+SUCCESS!
+=end
+
+number = 1
+
+100. times do 
+	if number % 3 == 0 && number % 5 ==0
+		puts "SUCCESS!"
+	elsif number % 3 == 0
+		puts "THREE"
+	elsif number % 5 == 0
+		puts "FIVE"
+	else 
+		puts number
+	end
+
+	number = number + 1
+	
+end
